7820dfa4425c688ccdb4be5d4839a1954ea69415
Celeste-Rhoades <celesteisthebest82@gmail.com>
2025-12-17 13:50:59 -0700

    added homescreen

--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { colors } from "../utils/theme";
+import { useAuthStore } from "../store/authStore";
+import { TouchableOpacity, Alert } from "react-native";
 
 const HomeScreen = () => {
+  const user = useAuthStore(state => state.user);
+  const logout = useAuthStore(state => state.logout);
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home Screen - You're logged in!</Text>
